2cf15312e515bc10b990d42ccc1dde9db286361f
Solomon Wise <susicnews@gmail.com>
2012-04-08 13:00:47 -0300

    Update python/MIT Classes/ps0.py

--- python/MIT Classes/ps0.py
+++ python/MIT Classes/ps0.py
@@ -3,12 +3,9 @@
  # Time: 3:30
 
 # ask the user to enter his/her last name
-print "What is your last name?",
-lastName = raw_input()
+raw_input("What is your last name?" )
 
 # ask the user to enter his/her first name
-print "What is your first name?",
-firstName = raw_input()
-
+raw_input("What is your first name?" )
 # Print out the user's first and last name in that order
 print firstName, lastName
