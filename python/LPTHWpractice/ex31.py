163418eda5d25a993a97142dfac248eb7db31fff
DK Monroe <david.dmonroe@gmail.com>
2013-02-23 10:38:40 -0500

    Redoing ex31.py for practice.

--- python/LPTHWpractice/ex31.py
+++ python/LPTHWpractice/ex31.py
@@ -6,7 +6,7 @@ if door == "1":
 	print "There's a giant bear here eating a cheese cake. What do you do?"
 	print "1. Take the cake."
 	print "2. Scream at the bear."
-	
+
 	bear = raw_input("> ")
 
 	if bear == "1":
@@ -14,7 +14,8 @@ if door == "1":
 	elif bear == "2":
 		print "The bear eats your legs off. Good job!"
 	else:
-		print "Well, diong %s is probably better. Bear runs away." % bear
+		print "Well, doing %s is probably better. Bear runs away." % bear
+
 elif door == "2":
 	print "You stare into the endless abyss at Cthulhu's retina."
 	print "1. Blueberries."
@@ -24,7 +25,7 @@ elif door == "2":
 	insanity = raw_input("> ")
 
 	if insanity == "1" or insanity == "2":
-		print "Your body survives powered by a mind of jello. Good job!"
+		print "You body survies powered by a mind of jello. Good job!"
 	else:
 		print "The insanity rots your eyes into a pool of muck. Good job!"
 
