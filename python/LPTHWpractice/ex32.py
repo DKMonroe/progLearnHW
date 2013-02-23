677fef4206c8e66fbbc54000f0325cd96db48367
DK Monroe <david.dmonroe@gmail.com>
2013-02-23 11:00:32 -0500

    Redoing 32 and 33.

--- python/LPTHWpractice/ex32.py
+++ python/LPTHWpractice/ex32.py
@@ -4,26 +4,26 @@ change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
 
 # this first kind of for-loop goes through a list
 for number in the_count:
-    print "This is count %d" % number
+	print "This is count %d" % number
 
 # same as above
-for fruit in fruits: 
-    print "A fruit of type: %s" % fruit
+for fruit in fruits:
+	print "A fruit of type: %s" % fruit
 
-# also we can go through mixed lists too
-# notice we have to use %r since we don't know what's in it
-for i in change: 
-    print "I got %r" % i
+#also we can go through mixed lists too
+#notice we have to use %r since we don't know what's in it
+for i in change:
+	print "I got %r" % i
 
 # we can also build lists, first start with an empty one
 elements = [ ]
 
 # then use the range function to do 0 to 5 counts
 for i in range(0, 6):
-    print "Adding %d to the list." % i
-    # append is a function that lists understand
-    elements.append(i)
-    
-# now we can print them out oo
+	print "Adding %d to the list." % i
+	#append is a function that lists understand
+	elements.append(i)
+
+# now we can print them out too
 for i in elements:
-    print "Element was: %d" % i
+	print "Element was: %d" % i
