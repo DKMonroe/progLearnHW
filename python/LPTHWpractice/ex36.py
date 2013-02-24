2acb43d4e2e8f8fd2abd9ad54702119f66e30c5f
DK Monroe <david.dmonroe@gmail.com>
2013-02-24 16:04:42 -0500

    Fleshing out stairway, armory, etc.

--- python/LPTHWpractice/ex36.py
+++ python/LPTHWpractice/ex36.py
@@ -26,16 +26,87 @@ def engine_room():
 # it has guns, armored vests
 # the user can pick these up and put them in their inventory
 def armory_room():
-	print "[enter description]"
-	print "[enter choices]"
-
+	print "You enter a dead end room"
+	print "but it seems to be an armory."
+	print "there are guns and armored vests"
+	print "wil you take them?"
+	print "type yes if you do, type no if you wish to return to the engine room"
+	
+	next = raw_input("> ")
+ #enter in the options for inventory when we figure out how to
+ #it will be a list to show items we pick up along the journey
+ #the player will have to be able to access the inventory at any time   
+    
 
 
 # stairway is a room that only has an exit up
 def stairway_room():
-	print "[enter description]"
+	print "you enter and see a staircase, a cramped foggy area surrounds you."
+	print "do you go up the stairs or go back?"
+	print " type up to go up the staircase type back to return to the enine room"
+	
+	next = raw_input("> ")
+	
+	if "up" in next:
+		bridge_room()
+	elif "back" in next:
+		engine_room()
+	else:
+		print "I do not understand this command"
+		stairway_room()
 
+#the bridge is a room that consists of a mini game
+#and a elevator that goes only up
 
+def bridge_room():
+	print "you enter the bridge. you look and see a vast open area."
+	print "there are windows and you stare at the vast open space."
+	print "there is a control pannel infront of you"
+	print "do you go and explore?"
+	print "type yes if you wish to explore, type no if you look around more."
+	
+	next = raw_input("> ")
+	
+	if "yes" in next:
+		control_game()
+	elif "no" in next:
+		observation_elevator()
+	else:
+		print"i dont understand this command"
+        
+#the control mini game is a game were you press numbers
+#a string of events happens when you press different numbers
+#a few things are obtained WE NEED TO FIGURE OUT THE INVENTORY!!!
+def control_game():
+	print "when you get to the control pannel you see a bunch of numbers"
+	print "the controls are simple press a number from 1-5 to see what happenes"
+	
+	next = raw_input("> ")
+	
+	if "1" in next:
+		print "a red and green parrot swoops down"
+		print "and lands on your shoulder. sings merry christmas and flys away"
+	elif "2" in next:
+		print"a cookie is obtained"
+	elif "3" in next:
+		print "a parrot flies to your shoulder."
+		print "you obtain it"
+	elif "4" in next:
+		print "sparks fly everywere!!"
+		print "the pannel dies"
+		bridge_room()
+	elif "5" in next:
+		print "all of a sudden a voice like wind whispers in your ear"
+		print "you are captvated by it."
+		print "you break out of the trance"
+	elif "go back" in next:
+		bridge_room()
+	else:
+		print"i dont understand this command"
+ 
+ 
+ 
+ 
 # a need user controls
 # the user can enter directions (n, s, e, w)
 # the user can pickup items (add to list)
