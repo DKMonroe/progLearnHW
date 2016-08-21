e319013aacab6a38530efa65d92660298942dc20
David Monroe <david.dmonroe@gmail.com>
2016-08-20 21:40:34 -0400

    Exercise 38 and Euler Problem 2 attempt

--- python/ProjectEuler/problem2.py
+++ python/ProjectEuler/problem2.py
@@ -5,14 +5,18 @@
 
 #1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
 
-#By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+#By considering the terms in the Fibonacci sequence whose values 
+# do not exceed four million, find the sum of the even-valued terms.
 
 values = []
 count = 1
 firstTerm = 1
 secondTerm = 2
+evens = []
 
 values.append (firstTerm)
 values.append (secondTerm)
 
 print (values)
+
+while values < 4000000:
